2fde680197bf3d1038c488a58c8029b5ba332e42
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-02-15 12:25:30 +0900

    Committing

--- temp.cu
+++ temp.cu
@@ -1,45 +1,47 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 // Kernel function to add the elements of two arrays
 
-__global__ void hello(){
-  printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+__global__ void hello() {
+    printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
 }
 
-__global__ void add(float *a, float *b, int n){
-  for (int i = 0; i < n; i++)
-  {
-    a[i] = a[i] + b[i];
-  }
-  
+__global__ void add(float *a, float *b, int n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = a[i] + b[i];
+    }
 }
 
-int main(void){
+__global__ void sgemm_kernel(int M, int N, int K, float alpha, const float* A, const float* B, float beta, float *C){
+  const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+}
 
-  float *a, *b, *d_a, *d_b;
-  int N = 1<<12;
-  a = (float *)malloc(N*sizeof(float));
-  b = (float *)malloc(N*sizeof(float));
-  for(int i = 0; i < N; i++){
-    a[i] = i;
-    b[i] = 2 * i;
-  }
-  cudaMalloc((void**)&d_a, N*sizeof(float));
-  cudaMemcpy(d_a, a, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMalloc((void**)&d_b, N*sizeof(float));
-  cudaMemcpy(d_b, b, N*sizeof(float), cudaMemcpyHostToDevice);
+int main(void) {
+    float *a, *b, *d_a, *d_b;
+    int N = 1 << 12;
+    a = (float *)malloc(N * sizeof(float));
+    b = (float *)malloc(N * sizeof(float));
+    for (int i = 0; i < N; i++) {
+        a[i] = i;
+        b[i] = 2 * i;
+    }
+    cudaMalloc((void **)&d_a, N * sizeof(float));
+    cudaMemcpy(d_a, a, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&d_b, N * sizeof(float));
+    cudaMemcpy(d_b, b, N * sizeof(float), cudaMemcpyHostToDevice);
 
-  add<<<1, 1>>>(d_a, d_b, N);
-  cudaDeviceSynchronize();
-  cudaMemcpy(a, d_a, N*sizeof(float), cudaMemcpyDeviceToHost);
-  float error = 0;
-  for(int i = 0; i < N; i++){
-    error += fabs(a[i] - 3 * i);
-  }
-  printf("Error: %f\n", error);
-  cudaFree(d_a);
-  cudaFree(d_b);
-  free(a);
-  free(b);
-  return 0;
+    add<<<1, 1>>>(d_a, d_b, N);
+    cudaDeviceSynchronize();
+    cudaMemcpy(a, d_a, N * sizeof(float), cudaMemcpyDeviceToHost);
+    float error = 0;
+    for (int i = 0; i < N; i++) {
+        error += fabs(a[i] - 3 * i);
+    }
+    printf("Error: %f\n", error);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    free(a);
+    free(b);
+    
+    return 0;
 }
