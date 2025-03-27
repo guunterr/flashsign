a9aaaa783e1e50edb3b48a6c1f7e66f736edddc2
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-03-27 14:09:15 +0900

    Skip first kernel launch

--- do_profile.sh
+++ do_profile.sh
@@ -4,4 +4,4 @@ mkdir out
 rm out/*
 nvcc -arch=sm_70 -g -DNDEBUG --generate-line-info -O3 --ptxas-options=-v -o out/temp temp.cu
 ./out/temp 4 1 3 > out/output_$(date +%m-%d_%H:%M).txt
-ncu -f --set full --export out/profile_$(date +%m-%d_%H:%M) ./out/temp 4 0 1
+ncu -f --launch-skip 1 --set full --export out/profile_$(date +%m-%d_%H:%M) ./out/temp 4 0 1
