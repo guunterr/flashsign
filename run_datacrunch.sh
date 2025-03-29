927baa96ad1c2b0a0c6e0f514c0846c21f3910f0
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-03-29 10:44:00 +0900

    Fiddle with kernel 5 params (register pressure)

--- run_datacrunch.sh
+++ run_datacrunch.sh
@@ -12,4 +12,10 @@ ssh root@$1 "\
     chmod +x do_profile.sh; \
     ./do_profile.sh $2;"
 
-scp root@$1:~/flashsign/out/profile* root@$1:~/flashsign/out/test* root@$1:~/flashsign/out/benchmark* out/
+datetime = $(date +%m-%d_%H:%M:%S)
+
+mkdir profiles/$2
+mkdir profiles/$2/datetime
+mkdir profiles/$2/datetime/kernels
+scp root@$1:~/flashsign/out/profile* root@$1:~/flashsign/out/test* root@$1:~/flashsign/out/benchmark* profiles/$2/datetime
+scp root@$1:oot@$1:~/flashsign/kernels/* profiles/$2/datetime/kernels/
