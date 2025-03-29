e16d10ecc102ee2eb0bf92bc89d42c5254e02e66
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-03-29 11:49:10 +0900

    Some changes to the run script

--- do_profile.sh
+++ do_profile.sh
@@ -2,7 +2,8 @@
 nvidia-smi
 mkdir out
 rm out/*
-nvcc -arch=sm_70 -g -DNDEBUG --generate-line-info -O3 --ptxas-options=-v -o out/temp temp.cu
-./out/temp t $1 1 1 > out/test_$(date +%m-%d_%H:%M).txt
-./out/temp r $1 1 3 > out/benchmark_$(date +%m-%d_%H:%M).txt
-ncu -f --launch-skip 1 --set full --export out/profile_$(date +%m-%d_%H:%M) ./out/temp r $1 1 1
+datetime=$(date +%m-%d_%H:%M)
+nvcc -arch=sm_70 -g -DNDEBUG --generate-line-info -O3 --ptxas-options=-v -o out/temp temp.cu > compiler_log_$datetime.txt
+./out/temp t $1 1 1 > out/test_$datetime.txt
+./out/temp r $1 1 3 > out/benchmark_$datetime.txt
+ncu -f --launch-skip 1 --set full --export out/profile_$datetime ./out/temp r $1 1 1
