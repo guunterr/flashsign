d7a2418483b4623ea5823fd4bbb5a3aa36a4da5a
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-03-31 12:15:52 +0900

    Kernel 6 Vectorised Ops

--- run_datacrunch.sh
+++ run_datacrunch.sh
@@ -8,7 +8,7 @@ ssh root@$1 "\
     eval \$(ssh-agent -s); \
     ssh-add ~/flashsign_key; \ 
     git reset --hard origin/master; \
-    git pull origin kernel5; \
+    git pull origin kernel6; \
     chmod +x do_profile.sh; \
     ./do_profile.sh $2;"
 
