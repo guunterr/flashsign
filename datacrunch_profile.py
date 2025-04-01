f79cd28ebef2599e8288358ea73a4be815eb58f7
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-04-01 19:44:59 +0900

    Do H100s

--- datacrunch_profile.py
+++ datacrunch_profile.py
@@ -21,7 +21,7 @@ def run_profile(ip):
 
 if len(dc_client.instances.get()) == 0:
     print("Creating instance...")
-    instance = dc_client.instances.create(instance_type="1V100.6V", 
+    instance = dc_client.instances.create(instance_type="1H100.80S.30V", 
                                             image=OS_VOLUME_ID,
                                             ssh_key_ids=ssh_keys_ids,
                                             is_spot=True,
