d62e40cbf6f1e18080a4c70db8871325f4d1da73
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-04-02 14:16:33 +0900

    Run fewer profile attempts

--- do_profile.sh
+++ do_profile.sh
@@ -5,5 +5,5 @@ rm out/*
 datetime=$(date +%m-%d_%H:%M)
 nvcc -arch=sm_90 -lcublas -g -DNDEBUG --generate-line-info -O3 --ptxas-options=-v -o out/temp temp.cu > out/compiler_log_$datetime.txt
 ./out/temp t $1 256 1 > out/test_$datetime.txt
-./out/temp r $1 5 50 > out/benchmark_$datetime.txt
+./out/temp r $1 2 20 > out/benchmark_$datetime.txt
 ncu -f --launch-skip 1 --set full --export out/profile_$datetime ./out/temp r $1 1 1
