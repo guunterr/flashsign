9085ccc646e532bb018b1a3f3333d59e8a66d602
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-04-10 12:00:56 +0900

    Profile on bigger matrices

--- temp.cu
+++ temp.cu
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
     }
     if (argv[1][0] == 'r') {
         printf("Running flashsign_kernel %d for %d warmup and %d runs\n", kernel_number, warmup, runs);
-        time_flashsign<4096, 128>(kernel_number, 4096, warmup, runs);
+        time_flashsign<16384, 128>(kernel_number, 16384, warmup, runs);
     } else{
         printf("Testing flashsign kernel %d\n", kernel_number);
         test_flashsign<512, 128>(kernel_number, 512);
