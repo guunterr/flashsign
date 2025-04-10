49c1baf978834dc1cdbb7d6802e0f38c02c4eb1b
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-04-10 15:01:57 +0900

    profiling back on

--- do_profile.sh
+++ do_profile.sh
@@ -5,5 +5,5 @@ rm out/*
 datetime=$2
 nvcc -arch=sm_70 -lcublas -g -DNDEBUG --generate-line-info -O3 --ptxas-options=-v -o out/temp temp.cu > out/compiler_log_$datetime.txt
 ./out/temp t $1 1024 1 0.001 > out/test_$datetime.txt
-# ./out/temp r $1 2 20 > out/benchmark_$datetime.txt
-# ncu -f --launch-skip 1 --set full --export out/profile_$datetime ./out/temp r $1 1 1
+./out/temp r $1 2 20 > out/benchmark_$datetime.txt
+ncu -f --launch-skip 1 --set full --export out/profile_$datetime ./out/temp r $1 1 1
