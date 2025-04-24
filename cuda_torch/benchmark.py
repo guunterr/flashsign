84baca6d3e4444823e4608fef36ddd70b092f2cd
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-04-24 15:17:07 +0900

    Some changes

--- cuda_torch/benchmark.py
+++ cuda_torch/benchmark.py
@@ -2,9 +2,9 @@ import os, torch
 from importlib import reload
 print(torch.version.cuda)
 import kernels.loadSave
-X = torch.rand(1024, 1024, device='cuda', dtype=torch.half)
+X = torch.rand(64, 64, device='cuda', dtype=torch.half)
 Y = kernels.loadSave.load_save(X)
 print(torch.equal(X**2,Y))
 print(X.shape, Y.shape)
 print(X.dtype, Y.dtype)
-print(X[0,0], Y[0,0])
+print(X[:,:], Y[:,:])
