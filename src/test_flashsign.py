0e998880c5de606ea0fe8ebcfc1f72306ba7f8f5
Gerard Glowacki <gerardrglowacki@gmail.com>
2025-05-09 13:54:50 +0900

    Thing

--- src/test_flashsign.py
+++ src/test_flashsign.py
@@ -6,18 +6,12 @@ from time import perf_counter
 from importlib import reload
 print(torch.version.cuda)
 
-import kernels.flashsign_0_unfused
-reload(kernels.flashsign_0_unfused)
-
 def signed_attention(Q, K, V):
     S = torch.matmul(Q, torch.transpose(K, 0, 1))
-    return S
-    # L = torch.linalg.norm(S, ord=2, dim=1, keepdim=True)
-    # S = S/L
-    # return torch.matmul(S, V)
+    L = torch.linalg.norm(S, ord=2, dim=1, keepdim=True)
+    S = S/L
+    return torch.matmul(S, V)
 
-def flashsign(Q, K, V):
-    return kernels.flashsign_0_unfused.flashsign_unfused(Q, K, V)
 
 def test_flashsign(kernel, reference, X, Y, epsilon=0.01):
     torch.manual_seed(69)
@@ -61,31 +55,31 @@ if __name__ == "__main__":
     if sys.argv[1] == "0":
         def flashsign(Q, K, V):
             return signed_attention(Q,K,V)
-    elif sys.argv[1] == "1":
-        import kernels.flashsign_0_unfused
-        reload(kernels.flashsign_0_unfused)
-        def flashsign(Q, K, V):
-            return kernels.flashsign_0_unfused.flashsign_unfused(Q, K, V)
+    # elif sys.argv[1] == "1":
+    #     import kernels.flashsign_0_unfused
+    #     reload(kernels.flashsign_0_unfused)
+    #     def flashsign(Q, K, V):
+    #         return kernels.flashsign_0_unfused.flashsign_unfused(Q, K, V)
 
-    elif sys.argv[1] == "2":
-        import kernels.flashsign_2_half2
-        reload(kernels.flashsign_2_half2)
-        def flashsign(Q,K,V):
-            return kernels.flashsign_2_half2.flashsign_2(Q, K, V)
-    elif sys.argv[1] == "3":
-        import kernels.flashsign_3_pytorch
-        reload(kernels.flashsign_3_pytorch)
-        def flashsign(Q,K,V):
-            return kernels.flashsign_3_pytorch.flashsign_3(Q, K, V)
-    elif sys.argv[1] == "4":
-        with open("./kernels/flashsign_kernel4_torchbind.cu", "r") as torch_binding_file:
-            with open("./kernels/flashsign_kernel4_cuda.cu", "r") as kernel_file:
-                with open("./kernels/flashsign_kernel4.cu", "w") as write_file:
-                    write_file.write("\n" + kernel_file.read() + "\n\n" + torch_binding_file.read())
-        #     print(kernel_file.read())
-        import kernels.flashsign_kernel4
-        reload(kernels.flashsign_kernel4)
-        def flashsign(Q,K,V):
-            return kernels.flashsign_kernel4.flashsign_4(Q, K, V)
+    # elif sys.argv[1] == "2":
+    #     import kernels.flashsign_2_half2
+    #     reload(kernels.flashsign_2_half2)
+    #     def flashsign(Q,K,V):
+    #         return kernels.flashsign_2_half2.flashsign_2(Q, K, V)
+    # elif sys.argv[1] == "3":
+    #     import kernels.flashsign_3_pytorch
+    #     reload(kernels.flashsign_3_pytorch)
+    #     def flashsign(Q,K,V):
+    #         return kernels.flashsign_3_pytorch.flashsign_3(Q, K, V)
+    # elif sys.argv[1] == "4":
+    #     with open("./kernels/flashsign_kernel4_torchbind.cu", "r") as torch_binding_file:
+    #         with open("./kernels/flashsign_kernel4_cuda.cu", "r") as kernel_file:
+    #             with open("./kernels/flashsign_kernel4.cu", "w") as write_file:
+    #                 write_file.write("\n" + kernel_file.read() + "\n\n" + torch_binding_file.read())
+    #     #     print(kernel_file.read())
+    #     import kernels.flashsign_kernel4
+    #     reload(kernels.flashsign_kernel4)
+    #     def flashsign(Q,K,V):
+    #         return kernels.flashsign_kernel4.flashsign_4(Q, K, V)
     # test_flashsign(flashsign, signed_attention, 1024, 1024)
     benchmark_flashsign(flashsign, 108 * 128 * int(sys.argv[2]), 108 * 128 * int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
